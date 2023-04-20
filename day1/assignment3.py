51605d7fd448687e96f76ab3681e19e6e4ee49d7
Benedict <newlyf38@gmail.com>
2023-04-20 03:02:43 +0000

    done with the day one of coding

---
+++ day1/assignment3.py
@@ -0,0 +1,7 @@
+#Write a code to get the length of an inputed string by a user.
+
+print('Hello enter a name of a string')
+
+user_input = input()
+
+print('The lenght of the string is ' , len(user_input))
---
+++ day1/assignment5.py
@@ -0,0 +1,7 @@
+#Create a band name generator using a users pet name and hometown
+
+print('Hello and welcome to our band name generator')
+
+user_pet_name = input('What is the name of your first pet: \n')
+user_hometown = input('what is the name of your hometown: \n')
+print('Your band name can be ' + user_pet_name + ' ' + user_hometown)
